65486c942f938daaa2e06608fdfcd3310a4aec47
Siiaaa08 <salonibaruaa810@gmail.com>
2025-01-07 13:48:03 -0800

    Division

---
+++ Division.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Calculator{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double x = sc.nextDouble();
+    double y = sc.nextDouble();
+    System.out.println("Enter Operator");
+    char op = sc.next().charAt(0);
+    switch(op){
+       case '/':
+          System.out.println(x/y);
+          break;
+   
+       
+     }
+  }
+}
+						
+    
+
